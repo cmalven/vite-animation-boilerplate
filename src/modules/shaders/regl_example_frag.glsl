a010d3469f19f3475fae8995a961537fcf3166f1
cmalven <chris@malven.co>
2022-01-28 12:10:40 -0600

    Small update to regl frag shader.

--- src/modules/shaders/regl_example_frag.glsl
+++ src/modules/shaders/regl_example_frag.glsl
@@ -7,10 +7,17 @@ void main () {
     // Convert to coordinate system with 0,0 in bottom left and 1,1 in top right
     float x = smoothstep(-1.0, 1.0, v_position.x);
     float y = smoothstep(-1.0, 1.0, v_position.y);
+    vec2 pos = vec2(x, y);
 
-    float r = x;
-    float g = y;
+    // Set background color
+    float r = pos.x;
+    float g = pos.y;
     float b = abs(sin(time / 2.0));
+    vec3 color = vec3(r, g, b);
 
-    gl_FragColor = vec4(r, g, b, 1.0);
+    // Draw a circle in the center
+    float center_dist = distance(vec2(0.5, 0.5), pos);
+    color += 1.0 - smoothstep(0.3, 0.3, center_dist);
+
+    gl_FragColor = vec4(color, 1.0);
 }
